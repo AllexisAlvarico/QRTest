7fb923cd6585177724750fdc82583bb23bec3ac8
AllexisAlvarico <c00242855@itcarlow.ie>
2020-07-10 05:05:33 +0100

    Starting to work on QR test

---
+++ QR Test/main.cpp
@@ -0,0 +1,33 @@
+/// <summary>
+/// Name: Allexis Alvarico
+/// Date: 10/07/2020
+/// --------------------------------------------------------=
+/// Summary: Testing for QR code for a large projects in the 
+/// future(In terms of interactive teaching I wish to implement).
+/// ---------------------------------------------------------
+/// Estinmated time: 60 hours
+/// ---------------------------------------------------------
+/// References
+/// ---------------------------------------------------------
+/// Known Bugs: 
+/// ----------------------------------------------------
+/// </summary>  
+
+#include <iostream>
+#include <fstream>
+#include "SFML/Graphics.hpp"
+
+
+
+int main()
+{
+	std::string testing;
+
+	std::cout << "hello world I like big projects" << std::endl;
+	std::cin >> testing;
+
+	std::cout << "I hate coding T_T" << std::endl;
+	std::cout << testing << std::endl; 
+
+	system("PAUSE");
+}
